338d3b797a4dbb83e6174b2337b827c08d24a3c1
Jahdasha Flagg <jahdashaflagg@Jahdashas-MacBook-Pro.local>
2016-03-04 00:44:25 -0500

    javascript update

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,6 +2,7 @@ $( document ).ready(function(){
   var Random=Math.floor(Math.random()*101+19)
   // Selects a random number to be shown at the start of the game
   // Number should be should be between 19 - 120
+  //
   $('#randomNumber').text(Random);
   // Appending random number to the randomNumber id in the html doc
   //
